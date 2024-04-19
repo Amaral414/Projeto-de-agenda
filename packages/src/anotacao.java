57fb9bfbc03b4691ad2156c4ccb7bcba42e5da19
Gabriel Amaral <gabriel.amaral414@hotmail.com>
2024-04-18 22:56:39 -0300

    Notas
    Entrei na fase de notas, agora estou trabalhando no desenvolvimento da lógica para guardar notas nos usuários.

---
+++ packages/src/anotacao.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class anotacao {
+    String nome;
+    String data;// Usar formato de data
+    String nota;
+    
+    public anotacao(String nome, String data, String nota) {
+        this.nome = nome;
+        this.data = data;
+        this.nota = nota;
+    }
+
+
+    
+    
+}
